70317bae53aeb54b6302da9893217bea08fc416e
Harsh Thapa <harshthapa4@gmail.com>
2022-08-27 16:52:51 +0530

    Initial commit for Robofriends App for practice No further changes expected

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@gmail.com'        
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Harsh Thapa',
+        username: 'harsh221010',
+        email: 'harshthapa4@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Sumit Kumar',
+        username: 'Sumit1245',
+        email: 'doctorsahab@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Himanshu Pokhriyal',
+        username: 'himanshu1542',
+        email: 'himanshupokhriyal@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Lalit Chand',
+        username: 'chand',
+        email: 'chandlalit53@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Vidhi Thapa',
+        username: 'Vidhi',
+        email: 'vidhithapa1408@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Ravinder Bahadur',
+        username: 'Ravinder',
+        email: 'bahadurravinder@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Savitri Thapa',
+        username: 'Savitri',
+        email: 'savitrithapa2106@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Gopal Chettri',
+        username: 'Gopal',
+        email: 'gopalchettri@gmail.com'
+    }
+]
